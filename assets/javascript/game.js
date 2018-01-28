850993a7e7f9f304fafe9ec27537397cf11a9ab9
TANZ <john.tansor@gmail.com>
2018-01-27 23:24:29 -0600

    adds js v1 / css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,38 @@
-
+var playScore = 0; 
+var wins = 0;
+var losses = 0;
 
 function newGame() {
-    var player = 0;
-    var target = Math.floor(Math.random() *102) +19;
-    var ruby = Math.floor(Math.random() *12) +1;
-    var blue = Math.floor(Math.random() *12) +1;
-    var yellow = Math.floor(Math.random() *12) +1;
-    var purple = Math.floor(Math.random() *12) +1;
+    target = Math.floor(Math.random() *102) +19;
+    playScore = 0;
+    crystalArr = [Math.floor(Math.random() *12) +1, Math.floor(Math.random() *12) +1, Math.floor(Math.random() *12) +1, Math.floor(Math.random() *12) +1];
+    console.log(crystalArr);  
+    $("#targetScore").text(target); 
+
+    function resetAll() {
+        playScore = 0;
+        newGame();   
+    }
+    function addOn() {
+        var index = $(this).attr("data-num");
+        playScore += crystalArr[index]
+        $("#playerScore").text(playScore);
         
-    $("#targetScore").append(target);
+        console.log("player= ", playScore, "target= ", target);
+        if (playScore > target) {
+            losses++;
+            $("#lossCount").text("LOSSES : " + losses);
+            alert("YOU LOSE.");
+            resetAll();
+        } else if (playScore == target) {
+            wins++;
+            $("#winCount").text("WINS : " + wins);
+            alert("YOU WIN!");
+            resetAll();
+            
+        }        
+    }
+    $(".clickMe").on("click", addOn);
+}   
 
-    $("#ruby").on("click", function() {
-        player += ruby;
-    });
-    $("#blue").on("click", function(){
-        player += blue;
-    });
-    $("#yellow").on("click", function(){
-        player += yellow;
-    });
-    $("#purple").on("click", function(){
-        player += purple;
-        
-    });
-    console.log(player);
-
-
-  
-    
-
-}
 newGame();
